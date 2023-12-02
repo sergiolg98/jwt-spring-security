1b072e42166b549aa05fd215196ed2ed565e49a1
Sergio Laureano <sergiolaureano98@gmail.com>
2023-12-02 07:06:56 -0500

    Validation for token in filter and updating Security Context

--- src/main/java/com/sergioferlg/security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/sergioferlg/security/config/JwtAuthenticationFilter.java
@@ -7,9 +7,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -41,11 +43,25 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         userEmail =  jwtService.extractUsername(jwtToken);
 
         if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){ //check also if user is not authenticated
+            //The next method is implemented by ApplicationConfig on a lambda expression
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
 
+            if(jwtService.isTokenValid(jwtToken, userDetails)){
+                //object needed by Spring and SecurityContextHolder to update the security context
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+
+                authenticationToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+            }
         }
-
-
-
+        filterChain.doFilter(request, response);    //pass to the next filter
     }
 }
